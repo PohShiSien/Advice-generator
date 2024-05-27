48561dbb587f11517294919752a81d27398f305c
PohShiSien <pohshisien@gmail.com>
2024-05-27 16:05:39 +0800

    done

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Advice({ clicked, advice, adviceId }) {
     return (
-        <div className="shadow-2xl p-12 flex flex-col items-center justify-between bg-darkGrayishBlue h-1/2 w-11/12 desktop:w-2/5 rounded-2xl">
+        <div className="shadow-2xl p-12 flex flex-col items-center justify-between bg-darkGrayishBlue h-1/2 w-full desktop:w-2/5 rounded-2xl">
             <div>
                 <p className={clicked ? "text-neonGreen tracking-widest text-sm font-quoteWeight text-center" : "hidden"}>A D V I C E # {adviceId}</p>
             </div>
